39a99fd81ce60ebf4830f11ff5642f98d618edb3
Unknown <ezrajamez@gmail.com>
2017-08-09 14:47:21 -0800

    Fixed incorrect definitions because i'm blind.

--- NonSTL/BigNumber.cpp
+++ NonSTL/BigNumber.cpp
@@ -1,39 +1,35 @@
 #include <iostream>
 #include <vector> // Will add custom vector class later
 #include <algorithm>
+
 class BigNumber{
     private:
-        std::vector<uint64_t> buffer[];
+        std::vector<uint64_t> buffer;
         std::vector<uint64_t>::iterator uint64_tit;
     public:
 
-        BigNumber(std::vector<uint64_t> tab[]){
+        BigNumber(std::vector<uint64_t> tab){
             try{
-                buffer->resize(tab->size());
                 int i2 = 0;
-            for (uint64_tit = tab->begin(); uint64_tit != tab->end(); uint64_tit++, i2++){
+            for (uint64_tit = tab.begin(); uint64_tit != tab.end(); uint64_tit++, i2++){
                 //std::cout << *uint64_tit << ' ';
-                buffer->assign(i2, *uint64_tit);
+                buffer.push_back(*uint64_tit);
             }
             }catch(std::bad_alloc){
             std::cout << "Out Of Memory/Illegal Memory Access";
 
-            for (uint64_tit = tab->begin(); uint64_tit != buffer->end(); uint64_tit++)
+            for (uint64_tit = tab.begin(); uint64_tit != buffer.end(); uint64_tit++)
                 std::cout << *uint64_tit << ' ';
             }
         }
-        bool operator==(uint64_t a){
-            bool YN = true;
-            int i = 0;
-            for(uint64_tit = buffer->begin(); uint64_tit != buffer->end(); uint64_tit++, i++){
-                    if(i != buffer->size()){
-                        if(a - (buffer->at(i+1)) == (buffer->at(i))){
 
-                        }else{
-                            YN = false;
-                            std::cout << std::endl << (*uint64_tit+1) << std::endl << *uint64_tit;
+        bool operator==(uint64_t a){
+            bool YN = false;
+            int i = 0;
+            for(uint64_tit = buffer.begin(); uint64_tit != buffer.end(); uint64_tit++, i++){
+                        if(a - *uint64_tit++ == *uint64_tit){
+                            YN = true;
                         }
-                    }
             }
             return YN;
         }
@@ -43,11 +39,9 @@ class BigNumber{
 
 int main(){
 
-std::vector<uint64_t> placeholder = { 9223372036854775807, 9223372036854775807}; // Tested multiple ways, seems to work(Only with 2*, will be fixed later)
-BigNumber a(&placeholder);
-if(a == (9223372036854775807*2)){
-    std::cout << ":thunk:" << std::endl;
-    return 1;
-}
+std::vector<uint64_t> placeholder = { 4, 4}; // Tested multiple ways, seems to work(Only with 2*, will be fixed later)
+BigNumber a(placeholder);
+
+
 return 0;
 }
