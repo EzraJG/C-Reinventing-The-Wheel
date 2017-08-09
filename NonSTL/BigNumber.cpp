9c9dfec22c36fed8f821ca08951c4eecf3c44d09
Unknown <ezrajamez@gmail.com>
2017-08-09 12:51:22 -0800

    Latest update(WIP)

--- NonSTL/BigNumber.cpp
+++ NonSTL/BigNumber.cpp
@@ -1,25 +1,33 @@
 #include <iostream>
-
+#include <vector> // Will add custom vector class later
+#include <algorithm>
 class BigNumber{
     private:
-        uint64_t buffer[2];
+
     public:
-        BigNumber(uint64_t tab[]){
-            buffer[0] = tab[0];
-            buffer[1] = tab[1];
+        std::vector<uint64_t> buffer[];
+        BigNumber(std::vector<uint64_t> tab[]){
+            for(int i = 0; i < tab->size(); i++){
+                buffer[i] = tab[i];
+            }
         }
         bool operator==(auto a){
-            if(a - buffer[1] == buffer[0]){
-                return true;
-            }else{return false;}
+            bool isEqual = false;
+            for(int i = 0; i < buffer->size()/8; i++){
+                if(a - buffer.at(i+1) == buffer.at(i)){
+                    isEqual = true;
+                }
+            }
+            return isEqual;
         }
+
 
 };
 
 int main(){
 
-uint64_t placeholder[2] = {9223372036854775807, 1}; // Tested multiple ways, seems to work!
-BigNumber a(placeholder);
+std::vector<long long unsigned int> placeholder = {9223372036854775807, 1}; // Tested multiple ways, seems to work!
+BigNumber a(&placeholder);
 if(a == 9223372036854775808){
     std::cout << ":thunk:" << std::endl;
 }
